8dc46bef6283869ca1489b5f24094fbca2876994
Ben <benjburkholder@gmail.com>
2019-01-30 16:17:54 -0500

    add comments

--- pagespeed-api.py
+++ pagespeed-api.py
@@ -1,15 +1,15 @@
 import requests
 import json
 
-# Docs: https://developers.google.com/speed/docs/insights/v5/get-started
+# Documentation: https://developers.google.com/speed/docs/insights/v5/get-started
 
-with open('pagespeed.txt') as pagespeedurls:
+with open('pagespeed.txt') as pagespeedurls: # Create a local file called 'pagespeed.txt' and populate it with URLs to test
     content = pagespeedurls.readlines()
     content = [line.rstrip('\n') for line in content]
 
     for line in content:
-        file = open('pagespeed-results.csv', 'a')
-        x = f'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={line}'
+        file = open('pagespeed-results.csv', 'a') # csv will be created automatically on local machine
+        x = f'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={line}' # This is the google pagespeed api url structure, using for loop to insert each url in .txt file
         r = requests.get(x)
         final = r.json()
         urlid = final['id']
@@ -19,5 +19,5 @@ with open('pagespeed.txt') as pagespeedurls:
         file.write(ID + '\n')
         file.write(FCP + '\n')
 
-        #print(ID) Uncomment to test success
-        #print(FCP) Uncomment to test success
+        #print(ID) Uncomment to test data population
+        #print(FCP) Uncomment to test data population
