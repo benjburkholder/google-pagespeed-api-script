68e918292475413d6c8d54583cde168e388a1eed
Ben <benjburkholder@gmail.com>
2019-01-30 16:00:20 -0500

    second commit

--- pagespeed-api.py
+++ pagespeed-api.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+# Docs: https://developers.google.com/speed/docs/insights/v5/get-started
 
 with open('pagespeed.txt') as pagespeedurls:
     content = pagespeedurls.readlines()
@@ -8,7 +9,7 @@ with open('pagespeed.txt') as pagespeedurls:
 
     for line in content:
         file = open('pagespeed-results.csv', 'a')
-        x = f'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={line}'     
+        x = f'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={line}'
         r = requests.get(x)
         final = r.json()
         urlid = final['id']
@@ -17,8 +18,6 @@ with open('pagespeed.txt') as pagespeedurls:
         FCP = f'First Contentful Paint: {urlfcp}'
         file.write(ID + '\n')
         file.write(FCP + '\n')
-        print(ID)
-        print(FCP)
 
-
-
+        #print(ID) Uncomment to test success
+        #print(FCP) Uncomment to test success
