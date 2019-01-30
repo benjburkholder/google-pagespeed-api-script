c94077c670afdc8b3f2cc600102a92889f51b89b
Ben <benjburkholder@gmail.com>
2019-01-30 18:05:50 -0500

    add new data point

--- pagespeed-api.py
+++ pagespeed-api.py
@@ -8,7 +8,7 @@ with open('pagespeed.txt') as pagespeedurls: # Create a local file called 'pages
     content = [line.rstrip('\n') for line in content]
 
     for line in content:
-        file = open('pagespeed-results.csv', 'a') # csv will be created automatically on local machine
+        file = open('pagespeed-results.txt', 'a') # csv will be created automatically on local machine
         x = f'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={line}' # This is the google pagespeed api url structure, using for loop to insert each url in .txt file
         r = requests.get(x)
         final = r.json()
@@ -16,8 +16,13 @@ with open('pagespeed.txt') as pagespeedurls: # Create a local file called 'pages
         ID = urlid
         urlfcp = final['loadingExperience']['metrics']['FIRST_CONTENTFUL_PAINT_MS']
         FCP = f'First Contentful Paint: {urlfcp}'
+        urlinput = final['loadingExperience']['metrics']['FIRST_INPUT_DELAY_MS']
+        INPUT = f'First Input Delay: {urlinput}'
         file.write(ID + '\n')
         file.write(FCP + '\n')
+        file.write(INPUT + '\n')
+        file.close()
 
-        #print(ID) Uncomment to test data population
-        #print(FCP) Uncomment to test data population
+        #print(ID) 
+        #print(FCP)
+        #print(INPUT)
