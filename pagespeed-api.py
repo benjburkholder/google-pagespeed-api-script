4d8123f686d260f65dcc78db1c49644de8f65a22
Ben <benjburkholder@gmail.com>
2019-01-31 08:42:32 -0500

    removed api key

--- pagespeed-api.py
+++ pagespeed-api.py
@@ -8,8 +8,8 @@ with open('pagespeed.txt') as pagespeedurls: # Create a local file called 'pages
     content = [line.rstrip('\n') for line in content]
 
     for line in content:
-        file = open('pagespeed-results.txt', 'a') # csv will be created automatically on local machine
-        x = f'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={line}?AIzaSyDcig6xGXax1iw02L5AyN0HRPfTUN_JSPY' # This is the google pagespeed api url structure, using for loop to insert each url in .txt file
+        file = open('pagespeed-results.csv', 'a') # csv will be created automatically on local machine
+        x = f'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={line}?api-key-here' # This is the google pagespeed api url structure, using for loop to insert each url in .txt file
         r = requests.get(x)
         final = r.json()
         urlid = final['id']
